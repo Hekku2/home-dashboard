7e18e38d5f0b8bb4a363c554bfbccea9e38baab1
Hekku2 <hekku2@gmail.com>
2016-01-06 21:05:21 +0200

    Moved vagrant bashrc to file.

--- user-install.sh
+++ user-install.sh
@@ -1,13 +1,6 @@
 #Global NPM
 wget -O- https://raw.githubusercontent.com/glenpike/npm-g_nosudo/master/npm-g-nosudo.sh | sh
-
-export NPM_PACKAGES="/home/vagrant/.npm-packages"
-export NODE_PATH="$NPM_PACKAGES/lib/node_modules${NODE_PATH:+:$NODE_PATH}"
-export PATH="$NPM_PACKAGES/bin:$PATH"
-# Unset manpath so we can inherit from /etc/manpath via the `manpath`
-# command
-unset MANPATH  # delete if you already modified MANPATH elsewhere in your config
-export MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
+cat vagrant-bashrc-additions.txt >> .bashrc
 
 source ~/.bashrc
 
