6f05c2547d4815c203e9b5fbcc96b691b257fa0e
Hekku2 <hekku2@gmail.com>
2016-01-08 08:48:05 +0200

    Added mysql install to init

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,11 @@ apt-get -qq update
 apt-get -qq install -y git
 apt-get -qq install python-software-properties python g++ make
 
+#Database configuration
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+apt-get -qq install -y mysql-server
+
 #Node js
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 apt-get -qq install -y nodejs build-essential
