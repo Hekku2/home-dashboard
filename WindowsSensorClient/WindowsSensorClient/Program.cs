fa62ed0698add70eff44265c7d58ee3a58beb3bf
Hekku2 <hekku2@gmail.com>
2016-01-10 21:16:13 +0200

    Windeow client for sensor measurement communication.

---
+++ WindowsSensorClient/WindowsSensorClient/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace WindowsSensorClient
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var client = new WebServiceClient("http://localhost:1337", "admin", "adminadminadmin", 2))
+            {
+                client.Authenticate();
+                client.SendTemperature(20);
+            }
+        }
+    }
+}
